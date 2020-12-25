ea6d95ab4d58f9765852d4ce66a2e83836463761
cherish8513 <cherish8513@naver.com>
2020-12-25 18:23:14 +0900

    update

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -9,23 +9,31 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
     MemberRepository currentMemberRepository;
+
+    @Bean
     public MemberService memberService() {
         return  new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public MemberRepository memberRepository(){
         if(currentMemberRepository == null)
             currentMemberRepository = new MemoryMemberRepository();
         return currentMemberRepository;
     }
+    @Bean
     public DiscountPolicy discountPolicy() {
         return new RateDiscountPolicy();
     }
